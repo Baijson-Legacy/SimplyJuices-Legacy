396bcdfde8a8b479bc0b2379f4e2d3ddfdd054a5
Baijson <dev@baijbot.tv>
2017-05-08 17:04:07 +0200

    bumped version

--- src/main/java/mod/baijson/simplyjuices/SimplyJuices.java
+++ src/main/java/mod/baijson/simplyjuices/SimplyJuices.java
@@ -21,7 +21,7 @@ public class SimplyJuices {
     public static final String MODID = "simplyjuices";
     public static final String MODNM = "Simply Juices";
 
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.1.2";
 
     public static final String PROXY_COMMON = "mod.baijson." + MODID + ".common.CommonProxy";
     public static final String PROXY_CLIENT = "mod.baijson." + MODID + ".client.ClientProxy";
